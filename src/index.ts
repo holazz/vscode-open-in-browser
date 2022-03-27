552c72be49d6fb19e530e3695169c3250abd2dae
holazz <2418184580@qq.com>
2022-03-27 12:02:55 +0800

    fix: no workspace file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import type { ExtensionContext } from 'vscode'
 import { commands } from 'vscode'
 import open from 'open'
-import { getActiveFilePath, getConfig, getOpenOptions } from './utils'
+import { getActiveFilePath, getActiveFileWorkspace, getConfig, getOpenOptions } from './utils'
 import { Server } from './server'
 
 export function activate(ctx: ExtensionContext) {
   ctx.subscriptions.push(
     commands.registerCommand('open-in-browser.open', async () => {
       const { defaultBrowser, useLocalServer } = getConfig('open-in-browser')
-      const target = useLocalServer ? await Server.create() : getActiveFilePath()
+      const target = useLocalServer && getActiveFileWorkspace() ? await Server.create() : getActiveFilePath()
       open(target, getOpenOptions(defaultBrowser))
     })
   )
